50ea16b6e1fde55ef53e1d713e03ae2dd3b35032
XabierHeguy <xabier.heguy@wanadoo.fr>
2022-12-12 15:25:59 +0100

    ajout ui3

--- src/app/ui/components/ui3/ui3.component.ts
+++ src/app/ui/components/ui3/ui3.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-ui3',
   templateUrl: './ui3.component.html',
-  styleUrls: ['./ui3.component.scss']
+  styleUrls: ['./ui3.component.scss'],
 })
 export class Ui3Component {
+  public close: boolean = true;
 
+  public toggle(): void {
+    this.close = !this.close;
+  }
 }
